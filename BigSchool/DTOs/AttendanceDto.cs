15bdd02cf86a147b741891d6c51e840501b9aeea
Dell E7470 <Dell E7470@DESKTOP-AC5UT69>
2021-07-12 16:28:31 +0700

    1207

---
+++ BigSchool/DTOs/AttendanceDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using BigSchool.Models;
+
+namespace BigSchool.DTOs
+{
+    public class AttendanceDto
+    {
+        public int CourseId { get; set; }
+    }
+}
--- BigSchool/ViewModels/CourseViewModel.cs
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using BigSchool.Models;
@@ -8,11 +9,26 @@ namespace BigSchool.ViewModels
 {
     public class CourseViewModel
     {
+        [Required]
+        public int Id { get; set; }
         public string Place { get; set; }
+         [Required]
+         [FutureDate]
         public string Date { get; set; }
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+        [Required]
         public byte Category { get; set; }
         public IEnumerable<Category> Categories { get; set; }
+        public string Heading { get; set; }
+
+        public string Action
+        {
+            get { return (Id != 0) ? "Update" : "Create"; }
+        }
+        public bool ShowAction { get; set; }
+        public IQueryable<Course> UpcommingCourses { get; internal set; }
 
         public DateTime GetDateTime()
         {
